63aee68c26be0d775fb2693672650d5acd5b84a1
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2019-03-25 10:38:57 -0300

    Enable hot reloading if DEBUG is set

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,4 +1,7 @@
 import responder
+import uvicorn
+import typing
+import os
 
 api = responder.API()
 
@@ -7,4 +10,10 @@ async def greet_world(req, resp, *, greeting):
     resp.text = f"{greeting}, world!"
 
 if __name__ == '__main__':
-    api.run()
+    debug = os.getenv('DEBUG')
+    if debug:
+        port: typing.Optional[int] = int(typing.cast(int, os.getenv('PORT'))) or 8000
+        print('-'*80 + '\nDebug is enabled\nHot reloading enabled\n' + '-'*80)
+        uvicorn.run('api:api', host="0.0.0.0", port=port, reload=True)
+    else:
+        api.run()
